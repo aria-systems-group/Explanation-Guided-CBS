122b57a7659fbcf5903da70459034983eb4c0cbd
Justin Kottinger <justin.kottinger@colorado.edu>
2021-11-09 06:51:23 -0700

    extended main to current capabilities.

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,124 @@
 // my includes
-#include "../includes/Astar.h"
+#include "../includes/Benchmark.h"
 #include "../includes/Cbs.h"
-#include "../includes/XG-Astar-H.h"
-#include "../includes/XG-Astar.h"
-#include "../includes/XG-CBS.h"
-#include "../includes/parseYAML.h"
 #include <iostream>
 #include <string>
 
 
 int main(int argc, char** argv) { 
 	
-	// arguments (in order) <executable> <High-Level Algorithm> <Low-Level Algorithm> <filename>.yaml <computation time> <explanation cost>
+	// arguments (in order) <executable> <expType> <High-Level Algorithm> <Low-Level Algorithm> <filename>.yaml <computation time> <explanation cost>
 
-	if (argc >= 5)
+	if (argc >= 6)
 	{
-		const std::string cbsType(argv[1]);  // {CBS, XG-CBS}
-		const std::string aStarType(argv[2]);  // {A, XG-A, XG-A-H}
-		const std::string inputYaml(argv[3]);  // {<path/fileName>.yaml}
-		const double planningTime = atof(argv[4]);  // { real number > 0 }
+		const std::string expType(argv[1]);  // {Plan, Benchmark, Multi-Benchmark}
+		const std::string cbsType(argv[2]);  // {CBS, XG-CBS}
+		const std::string aStarType(argv[3]);  // {A, XG-A, XG-A-H}
+		const std::string inputYaml(argv[4]);  // {<path/fileName>.yaml, <path/to/multiple/yaml/files>}
+		const double planningTime = atof(argv[5]);  // { real number > 0 }
 
-		if ( (cbsType == "XG-CBS" && argc == 6) || (cbsType == "CBS" && argc == 5))
+		if ( (cbsType == "XG-CBS" && argc == 7) || (cbsType == "CBS" && argc == 6))
 		{
-			// create environment from yaml file
-			Environment *mapf = yaml2env(inputYaml);
-		
-			// init solution (to be filled by planning)
-			Solution solution;
-			bool success;
-
-			if (cbsType == "CBS" && aStarType == "A")
+			if (expType == "Plan")
 			{
-				CBS *planner = new CBS(mapf);
-				planner->setSolveTime(planningTime);
-				success = planner->plan(mapf->getStarts(), solution);
+				// create environment from yaml file (assumed to be a file)
+				Environment *mapf = yaml2env(inputYaml);
 
-				planner->clear();
-			}
-			else if (cbsType == "XG-CBS")
-			{
-				const int costBound = atoi(argv[5]);  // computation time
-				XG_CBS *planner = new XG_CBS(mapf, costBound);
-				planner->setSolveTime(planningTime);
-				// last two args are "useXG", and "useHeuristics"
-				if (aStarType == "A")
-					success = planner->plan(mapf->getStarts(), solution, false, false);
-				else if (aStarType == "XG-A")
-					success = planner->plan(mapf->getStarts(), solution, true, false);
-				else if (aStarType == "XG-A-H")
-					success = planner->plan(mapf->getStarts(), solution, true, true);
+				std::string dir2file = inputYaml.substr(inputYaml.rfind("/")+1);
+				const std::string::size_type end = dir2file.find(".yaml");
+				const std::string mapName = dir2file.erase(end, dir2file.length());
+
+				mapf->setMapName(mapName);
+
+				// init solution (to be filled by planning)
+				Solution solution;
+				bool success = false;
+
+				if (cbsType == "CBS" && aStarType == "A")
+				{
+					CBS *planner = new CBS(mapf);
+					planner->setSolveTime(planningTime);
+					success = planner->plan(mapf->getStarts(), solution);
+				}
+				else if (cbsType == "XG-CBS")
+				{
+					const int costBound = atoi(argv[6]);  // computation time
+					XG_CBS *planner = new XG_CBS(mapf, costBound);
+					planner->setSolveTime(planningTime);
+					// last two args are "useXG", and "useHeuristics"
+					if (aStarType == "A")
+						success = planner->plan(mapf->getStarts(), solution, false, false);
+					else if (aStarType == "XG-A")
+						success = planner->plan(mapf->getStarts(), solution, true, false);
+					else if (aStarType == "XG-A-H")
+						success = planner->plan(mapf->getStarts(), solution, true, true);
+					else
+						printf("Terminating prematurely due to invalid arguments.\n");
+				}
 				else
 					printf("Terminating prematurely due to invalid arguments.\n");
 
-				planner->clear();
+				// we have planned, now output solution to file if applicable
+				if (success)
+				{
+					
+					const std::string output_name = "txt/" + mapName + "_sol.txt";
+					std::ofstream out(output_name);
+					std::cout << "Outputting Solution to: " << output_name << std::endl;
+					for (std::vector<State*> agentSol: solution)
+					{
+						int it = std::distance(solution.begin(), 
+							std::find(solution.begin(), solution.end(), agentSol));
+						out << mapf->getAgentNames()[it] << std::endl;
+						for (State *st: agentSol)
+							out << *st << std::endl;
+					}
+				}
+			}
+			else if (expType == "Benchmark")
+			{
+				// create environment from yaml file (assumed to be a file)
+				Environment *mapf = yaml2env(inputYaml);
+
+				std::string dir2file = inputYaml.substr(inputYaml.rfind("/")+1);
+				const std::string::size_type end = dir2file.find(".yaml");
+				const std::string mapName = dir2file.erase(end, dir2file.length());
+
+				mapf->setMapName(mapName);
+
+				if (cbsType == "XG-CBS")
+				{
+					const int costBound = atoi(argv[6]);  // computation time
+					if (aStarType == "XG-A-H")
+					{
+						std::vector<std::pair <std::string, std::vector<std::string>> > data = singleMapBenchmark(mapf, costBound, planningTime);
+						write_csv("XG_CBS_Benchmark.csv", data);
+
+					}
+					else
+						printf("No benchmark function for %s.\n", aStarType.c_str());
+				}
+				else
+					printf("No benchmark function for %s.\n", cbsType.c_str());
+			}
+			else if (expType == "MultiBenchmark")
+			{
+				if (cbsType == "XG-CBS")
+				{
+					const int costBound = atoi(argv[6]);  // computation time
+					if (aStarType == "XG-A-H")
+					{
+						std::vector<std::pair <std::string, std::vector<std::string>> > data = multiMapBenchmark(inputYaml, costBound, planningTime);
+						write_csv("XG_CBS_Benchmark.csv", data);
+					}
+					else
+						printf("No multi-benchmark function for %s.\n", aStarType.c_str());
+				}
+				else
+					printf("No multi-benchmark function for %s.\n", aStarType.c_str());
 			}
 			else
 				printf("Terminating prematurely due to invalid arguments.\n");
-
-			// we have planned, now output solution to file if applicable
-			if (success)
-			{
-				std::string dir2file = inputYaml.substr(inputYaml.rfind("/")+1);
-				std::string fileType = ".yaml";
-				std::string::size_type end = dir2file.find(fileType);
-				std::string output_name = "txt/" + dir2file.erase(end, dir2file.length()) + "_sol.txt";
-				std::ofstream out(output_name);
-				std::cout << "Outputting Solution to: " << output_name << std::endl;
-				for (std::vector<State*> agentSol: solution)
-				{
-					int it = std::distance(solution.begin(), 
-						std::find(solution.begin(), solution.end(), agentSol));
-					out << mapf->getAgentNames()[it] << std::endl;
-					for (State *st: agentSol)
-						out << *st << std::endl;
-				}
-			}
 		}
 		else
 			printf("Terminating prematurely due to invalid arguments.\n");
